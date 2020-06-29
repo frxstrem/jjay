835038d75b771e4f9da275ca21bfdd9919e8806b
Fredrik Østrem <frx.apps@gmail.com>
2020-06-29 21:52:04 +0200

    More tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,6 +5,8 @@ make_test!(var: r"let x = 0; x" => "0");
 make_test!(func: r"let f(x) = x + 1; f(2)" => "3");
 make_test!(func2: r"let f(x)(y) = x * y + x + y; f(3)(4)" => "19");
 make_test!(pipe: r"let f(x)(y) = x + y; 1 | f(2)" => "3");
+make_test!(pipe_twice: r"let f(x) = x + 1; let g(x) = x * 2; 1 | f | g" => "4");
+make_test!(pipe_nested: r"let f(x)(y) = x + y; 1 | (2 | f)" => "3");
 make_test!(func_var: r"let f(x) = x * x; let g = f; g(3)" => "9");
 make_test!(arithmetic: "1 + 2 * 2 * 3 / 4 * 5 - 6 + 7" => "17");
 make_test!(func_noargs1: r"let f() = 3; f()" => "3");
